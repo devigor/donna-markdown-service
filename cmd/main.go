54d9f10bf878854f96bc4dad9fde6d681509ce15
Igor Oliveira <devigor.tere@gmail.com>
2023-08-17 16:07:11 -0300

    feat: initial commit with grpc server

---
+++ cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"github.com/devigor/donna-markdown-service/contracts"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	contracts.UnimplementedMarkdownServer
+}
+
+func (s *server) GetAll(ctx context.Context, request *contracts.Empty) (*contracts.GetAllResponse, error) {
+	data := []*contracts.MarkdownBody{
+		{
+			Id:        uuid.New().String(),
+			Content:   "# Title",
+			CreatedAt: &timestamp.Timestamp{Seconds: time.Now().Unix()},
+			UpdatedAt: &timestamp.Timestamp{Seconds: time.Now().Unix()},
+		},
+		{
+			Id:        uuid.New().String(),
+			Content:   "# Title 2",
+			CreatedAt: &timestamp.Timestamp{Seconds: time.Now().Unix()},
+			UpdatedAt: &timestamp.Timestamp{Seconds: time.Now().Unix()},
+		},
+		{
+			Id:        uuid.New().String(),
+			Content:   "# Title 3",
+			CreatedAt: &timestamp.Timestamp{Seconds: time.Now().Unix()},
+			UpdatedAt: &timestamp.Timestamp{Seconds: time.Now().Unix()},
+		},
+	}
+	return &contracts.GetAllResponse{
+		Items: data,
+	}, nil
+}
+
+var port = flag.Int("port", 50051, "The server port")
+
+func main() {
+	flag.Parse()
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v: ", err)
+	}
+
+	s := grpc.NewServer()
+	contracts.RegisterMarkdownServer(s, &server{})
+	log.Printf("server listening at %v", listen.Addr())
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/devigor/donna-markdown-service
+
+go 1.20
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230803162519-f966b187b2e5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230815205213-6bfd019c3878 // indirect
+	google.golang.org/grpc v1.57.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)

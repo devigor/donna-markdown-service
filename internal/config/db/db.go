94468fe4f21488401cbceb07532431010f07d18a
Igor Oliveira <devigor.tere@gmail.com>
2023-08-23 17:58:28 -0300

    fix: better database and env helper

---
+++ internal/config/db/db.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/devigor/donna-markdown-service/internal/config/env"
+	"github.com/jackc/pgx/v5"
+)
+
+func OpenConn() (*pgx.Conn, error) {
+	connStr := env.GetEnv("DATABASE_URL")
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	return conn, nil
+}
